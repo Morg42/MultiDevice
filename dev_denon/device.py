9ca31b3fb3c754fa1f547ad6fef57123cfea07b5
Morg42 <43153739+Morg42@users.noreply.github.com>
2021-12-19 22:22:28 +0100

    minor adjustments

--- dev_denon/device.py
+++ dev_denon/device.py
@@ -2,7 +2,7 @@
 # vim: set encoding=utf-8 tabstop=4 softtabstop=4 shiftwidth=4 expandtab
 
 '''
-Example class for Pioneer AV Power On function.
+Device class for Denon AV.
 '''
 if MD_standalone:
     from MD_Device import MD_Device
@@ -23,7 +23,7 @@ class MD_Device(MD_Device):
         # get MultiDevice.device logger
         self.logger = logging.getLogger('.'.join(__name__.split('.')[:-2]) + f'.{device_name}')
 
-        super().__init__(device_id, device_name, conn_type='net_tcp_client', terminator="b'\r'", timeout=6, blocking=0, command_class=MD_Command_ParseStr, **kwargs)
+        super().__init__(device_id, device_name, conn_type='net_tcp_client', terminator=b'\r', timeout=6, blocking=0, command_class=MD_Command_ParseStr, **kwargs)
 
         # log own initialization with module (i.e. folder) name
         self.logger.debug(f'device initialized from {__spec__.name} with arguments {kwargs}')

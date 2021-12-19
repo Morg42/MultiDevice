824ec4056ef809039f436186b679498b83cf8a4c
Onkel Andy <onkelandy@hotmail.com>
2021-12-19 22:27:52 +0100

    denon clean datatypes and lookup

--- dev_denon/datatypes.py
+++ dev_denon/datatypes.py
@@ -3,10 +3,8 @@
 
 if MD_standalone:
     import datatypes as DT
-    from dev_pioneer import lookup
 else:
     from .. import datatypes as DT
-    from . import lookup
 
 import re
 
@@ -49,92 +47,3 @@ class DT_onoff(DT.Datatype):
 
     def get_shng_data(self, data, type=None):
         return False if data == 'OFF' else True
-
-class DT_PioSource(DT.Datatype):
-    def get_send_data(self, data, type=None):
-        if data:
-            try:
-                data = int(data)
-                return f"{data:02}FN"
-            except Exception:
-                return f"{lookup.SOURCE_SET.get(data.upper())}FN"
-        else:
-            return '?FN'
-
-    def get_shng_data(self, data, type=None):
-        return_value = data.split("FN")[1]
-        return lookup.SOURCE.get(return_value)
-
-class DT_PioListening(DT.Datatype):
-    def get_send_data(self, data):
-        if data:
-            try:
-                data = int(data)
-                return f"{data:04}SR"
-            except Exception:
-                return f"{lookup.SOURCE_SET.get(data.upper())}SR"
-
-    def get_shng_data(self, data, type=None):
-        return_value = data.split("SR")[1]
-        return lookup.LISTENINGMODE.get(return_value)
-
-class DT_PioPlayingmode(DT.Datatype):
-    def get_shng_data(self, data, type=None):
-        return_value = data.split("LM")[1]
-        return lookup.PLAYINGMODE.get(return_value)
-
-class DT_PioDialog(DT.Datatype):
-    def get_send_data(self, data):
-        if data:
-            try:
-                data = int(data)
-                return f"{data:01}ATH"
-            except Exception:
-                return f"{lookup.DIALOG_SET.get(data.upper())}ATH"
-
-    def get_shng_data(self, data, type=None):
-        return_value = data.split("ATH")[1]
-        return lookup.DIALOG.get(return_value)
-
-class DT_PioHDMIOut(DT.Datatype):
-    def get_send_data(self, data):
-        if data:
-            try:
-                data = int(data)
-                return f"{data:01}HO"
-            except Exception:
-                return f"{lookup.DIALOG_SET.get(data.upper())}HO"
-
-    def get_shng_data(self, data, type=None):
-        return_value = data.split("HO")[1]
-        return lookup.DIALOG.get(return_value)
-
-class DT_PioPwr2(DT.Datatype):
-    def get_send_data(self, data):
-        return 'APO\rAPO' if data else 'APF'
-
-    def get_shng_data(self, data, type=None):
-        if type is None or type == 'bool':
-            return True if data == 'APR0' else False
-
-        return super().get_shng_data(data, type)
-
-class DT_PioMute2(DT.Datatype):
-    def get_send_data(self, data):
-        return 'Z2MO' if data else 'Z2MF'
-
-    def get_shng_data(self, data, type=None):
-        return True if data == 'Z2MUT0' else False
-
-class DT_PioSource2(DT.Datatype):
-    def get_send_data(self, data):
-        if data:
-            try:
-                data = int(data)
-                return f"{data:02}ZS"
-            except Exception:
-                return f"{lookup.SOURCE_SET.get(data.upper())}ZS"
-
-    def get_shng_data(self, data, type=None):
-        return_value = data.split("Z2F")[1]
-        return lookup.SOURCE.get(return_value)

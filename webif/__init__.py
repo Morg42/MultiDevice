8d5ee16736eb82051f67d797313997a4701d2e3d
Morg42 <43153739+Morg42@users.noreply.github.com>
2022-01-14 21:17:53 +0100

    update webif

--- webif/__init__.py
+++ webif/__init__.py
@@ -30,74 +30,7 @@ import json
 from lib.item import Items
 from lib.model.smartplugin import SmartPluginWebIf
 from ..MD_Globals import *
-
-
-# ------------------------------------------
-#    Webinterface of the plugin
-# ------------------------------------------
-
 import cherrypy
-
-
-# class WebInterface(SmartPluginWebIf):
-# 
-#     def __init__(self, webif_dir, plugin):
-#         """
-#         Initialization of instance of class WebInterface
-# 
-#         :param webif_dir: directory where the webinterface of the plugin resides
-#         :param plugin: instance of the plugin
-#         :type webif_dir: str
-#         :type plugin: object
-#         """
-#         self.logger = plugin.logger
-#         self.webif_dir = webif_dir
-#         self.plugin = plugin
-#         self.items = Items.get_instance()
-# 
-#         self.tplenv = self.init_template_environment()
-# 
-# 
-#     @cherrypy.expose
-#     def index(self, reload=None):
-#         """
-#         Build index.html for cherrypy
-# 
-#         Render the template and return the html file to be delivered to the browser
-# 
-#         :return: contents of the template after beeing rendered
-#         """
-#         tmpl = self.tplenv.get_template('index.html')
-#         # add values to be passed to the Jinja2 template eg: tmpl.render(p=self.plugin, interface=interface, ...)
-#         return tmpl.render(p=self.plugin,
-#                            items=sorted(self.items.return_items(), key=lambda k: str.lower(k['_path'])),
-#                            item_count=0)
-# 
-# 
-#     @cherrypy.expose
-#     def get_data_html(self, dataSet=None):
-#         """
-#         Return data to update the webpage
-# 
-#         For the standard update mechanism of the web interface, the dataSet to return the data for is None
-# 
-#         :param dataSet: Dataset for which the data should be returned (standard: None)
-#         :return: dict with the data needed to update the web page.
-#         """
-#         if dataSet is None:
-#             # get the new data
-#             data = {}
-# 
-#             # data['item'] = {}
-#             # for i in self.plugin.items:
-#             #     data['item'][i]['value'] = self.plugin.getitemvalue(i)
-#             #
-#             # return it as json the the web page
-#             # try:
-#             #     return json.dumps(data)
-#             # except Exception as e:
-#             #     self.logger.error("get_data_html exception: {}".format(e))
-#         return {}
 
 
 #############################################################################################################################################################################################################################################
@@ -144,12 +77,12 @@ class WebInterface(SmartPluginWebIf):
                 plgitems.append(item)
 
                 # print(type(item))
-                path_elems = item.id().split('.')
-                node = {path_elems[-1]: {a: getattr(item.property, a) for a in ['name', 'type', 'value', 'enforce_updates', 'eval', 'eval_trigger', 'on_change', 'on_update'] + item.property.attributes if hasattr(item.property, a) and (getattr(item.property, a) or getattr(item.property, a) is False)}}
-                for elem in reversed(path_elems[:-1]):
-                    node = {elem: node}
-
-                update(item_tree, node)
+                # path_elems = item.id().split('.')
+                # node = {path_elems[-1]: {a: getattr(item.property, a) for a in ['name', 'type', 'value', 'enforce_updates', 'eval', 'eval_trigger', 'on_change', 'on_update'] + item.property.attributes if hasattr(item.property, a) and (getattr(item.property, a) or getattr(item.property, a) is False)}}
+                # for elem in reversed(path_elems[:-1]):
+                #     node = {elem: node}
+                # 
+                # update(item_tree, node)
 
         return tmpl.render(p=self.plugin,
                            items=sorted(self.items.return_items(), key=lambda k: str.lower(k['_path'])),
@@ -157,8 +90,8 @@ class WebInterface(SmartPluginWebIf):
                            plgitems=plgitems,
                            running={dev: self.plugin._devices[dev]['device'].alive for dev in self.plugin._devices},
                            devices=self.plugin._devices,
-                           lookups={dev: self.plugin._devices[dev]['device']._commands._lookups for dev in self.plugin._devices},
-                           tree=json.dumps(item_tree))
+                           lookups={dev: self.plugin._devices[dev]['device']._commands._lookups for dev in self.plugin._devices})
+                           # tree=json.dumps(item_tree))
 
     @cherrypy.expose
     def submit(self, button=None, param=None):

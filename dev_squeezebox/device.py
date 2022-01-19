bb23a4c8a7f210f8e76a5d404716ddeaa9d55652
Morg42 <43153739+Morg42@users.noreply.github.com>
2022-01-19 10:51:25 +0100

    use callbacks

--- dev_squeezebox/device.py
+++ dev_squeezebox/device.py
@@ -24,6 +24,8 @@ class MD_Device(MD_Device):
 
     def _set_custom_vars(self):
         self.custom_commands = 1
+        self._use_callbacks = True
+
         # set our own preferences concerning connections
         if not self._params.get(PLUGIN_ATTR_CONNECTION):
             if PLUGIN_ATTR_NET_HOST in self._params and self._params.get(PLUGIN_ATTR_NET_HOST):

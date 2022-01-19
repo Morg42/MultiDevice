09afb8aba30a0f35a28dfbc6c3eb7ece697cdd3b
Onkel Andy <onkelandy@hotmail.com>
2022-01-19 10:26:47 +0100

    squeezebox: fix commands

--- dev_squeezebox/commands.py
+++ dev_squeezebox/commands.py
@@ -6,9 +6,9 @@
 
 commands = {
     'server': {
-        'listenmode': {'read': True, 'write': True, 'write_cmd': ':listen {VAL:01}:', 'item_type': 'bool', 'dev_datatype': 'str', 'reply_token': 'REGEX', 'reply_pattern': r'.*listen (\d)'}
+        'listenmode': {'read': True, 'write': True, 'write_cmd': ':listen {VAL:01}:', 'item_type': 'bool', 'dev_datatype': 'str', 'reply_token': 'REGEX', 'reply_pattern': r'listen (\d)'}
     },
     'playercontrol': {
-        'volume': {'read': True, 'write': True, 'read_cmd': 'mixer volume ?', 'item_type': 'num', 'write_cmd': ':mixer volume {VAL}:', 'item_type': 'num', 'dev_datatype': 'str', 'reply_token': 'REGEX', 'reply_pattern': r'.*mixer volume (\d)'}
+        'volume': {'read': True, 'write': True, 'read_cmd': 'mixer volume ?', 'item_type': 'num', 'write_cmd': ':mixer volume {VAL}:', 'item_type': 'num', 'dev_datatype': 'str', 'reply_token': 'REGEX', 'reply_pattern': r'.*mixer volume (\d{1,3})'}
     }
 }
